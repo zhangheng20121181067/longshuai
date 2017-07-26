4d8f0fd309aef6f4c3f1e0b96326de12e5b88f1f
zhangheng20121181067 <zhangheng93933@163.com>
2017-07-26 16:12:55 +0800

    1

--- js/index.js
+++ js/index.js
@@ -5,14 +5,13 @@ $(function(){
     var arr=["首页","关于我们","项目介绍","养生资讯","联系我们"];
     var swiper = new Swiper('.swiper-container', {
         loop:true,
-        initialSlide : GetQueryString('actSlideIndex')? GetQueryString('actSlideIndex'):0,  //初始slider
+     //   initialSlide :GetQueryString('actSlideIndex')? GetQueryString('actSlideIndex'):0,  //初始slider
         onSlideChangeEnd: function(swiper){
-          //  var $index = swiper.activeIndex;
+          // var $index = swiper.activeIndex;
             var $actSlide=$('.swiper-slide-active');
             var $index=$actSlide.attr('data-slide-index');
             $('.footer .tab li a').removeClass('clickAct').eq($index).addClass('clickAct');
 
-            console.log($index);
            /* //第一页或最后一页时禁止切换
             swiper.unlockSwipeToPrev();
             swiper.unlockSwipeToNext();
@@ -24,16 +23,25 @@ $(function(){
             }*/
         }
     });
+
+
+
     //点击时切换slider 并改变样式
     $('.footer .tab li').on('click',function(){
         var $this = $(this);
-        var $index = $this.index();
-        console.log("dianji"+$index);
-        swiper.slideTo($index+1);
-        $('.footer .tab li a').removeClass('clickAct').eq($index).addClass('clickAct');
+        var index = $this.index();
+        //swiper.slideTo(index);
+        swiper.slideTo(index+1);
+        $('.footer .tab li a').removeClass('clickAct').eq(index).addClass('clickAct');
 
         if($(this).find("a").hasClass("mouseAct")){
             $(this).find("a").removeClass("mouseAct")
         }
     });
+
+    //loop加上
+    var  actSlideIndex = GetQueryString('actSlideIndex');
+    if (actSlideIndex) {
+        $('.footer .tab li').eq(actSlideIndex).click();
+    }
 });
